a0bbaeeb323a04ede66dda1f00a408e72cee6744
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-12-15 16:07:36 -0300

    adicionando herança

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ByteBank.Funcionarios;
+using System;
 
 namespace ByteBank
 {
@@ -10,6 +7,31 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            GerenciadorBonificacao gerenciadorBonificacao = new GerenciadorBonificacao();
+
+            Funcionario funcionario = new Funcionario();
+            funcionario.Nome = "João";
+            funcionario.CPF = "125.246.146-12";
+            funcionario.Salario = 1000;
+
+            gerenciadorBonificacao.Registrar(funcionario);
+
+            Diretor diretor = new Diretor();
+            diretor.Nome = "Roberta";
+            diretor.CPF = "122.456.125-12";
+            diretor.Salario = 5000;
+
+            gerenciadorBonificacao.Registrar(diretor);
+
+            Console.WriteLine(funcionario.Nome);
+            Console.WriteLine(funcionario.GetBonificacao());
+
+            Console.WriteLine(diretor.Nome);
+            Console.WriteLine(diretor.GetBonificacao());
+
+            Console.WriteLine("Total da bonificação: " + gerenciadorBonificacao.getTotalBonificacao());
+
+            Console.ReadLine();
         }
     }
 }
